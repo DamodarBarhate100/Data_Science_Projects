35c26ca24bd00187fc47cfac62bce6eaa819b84a
Damodar Barhate <barhatedamodar25@gmail.com>
2026-03-23 23:22:52 +0530

    Implemented logistic regression using numpy from scratch

--- 04_Customer Churn Prediction/Scripts/logistic_regression.py
+++ 04_Customer Churn Prediction/Scripts/logistic_regression.py
@@ -1,8 +1,11 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from scratch_implementation_logistic_reg import Scratch_Logistic_Regression
 df = pd.read_csv("Datasets/customer_churn.csv")
 print(df.head())
 
@@ -80,3 +83,47 @@ plt.ylabel('Monthly Charges ($)')
 plt.tight_layout()
 plt.savefig("Visualizations/monthy_charges_spread.png")
 plt.close()
+
+
+# Removing  Non-Predictive Identifiers 
+new_df = df.drop(columns=['customerID'])
+
+# ________________________________________ Model Training and Testing ______________________________________
+# Separating Features (X) and Target (y)
+X = new_df[new_df.columns[:-1]]
+Y = new_df['Churn'].map({'Yes':1, 'No':0})
+
+X_train, X_test, y_train, y_test  = train_test_split(X,Y, test_size= 0.2,train_size=0.8,random_state=42, stratify=Y)
+
+X_train_encoded = pd.get_dummies(X_train,prefix="en",drop_first=True,dtype=int)
+X_test_encoded = pd.get_dummies(X_test,prefix="en",drop_first=True,dtype=int)
+
+log_reg = LogisticRegression()
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train_encoded) 
+X_test_scaled = scaler.transform(X_test_encoded)
+
+log_reg.fit(X_train_scaled, y_train)
+print("\nCoefficients:",log_reg.coef_)
+print("Intercept:",log_reg.intercept_)
+
+
+print("\n ____________________________Prediction using Sklearn Library___________________________")
+y_predicted = log_reg.predict(X_test_scaled) 
+print("\nFirst 10 Predictions:", y_predicted[:10])
+print("\n________Performance metrics_____________")
+print("Accuracy:", accuracy_score(y_test, y_predicted))
+print("F1 Score:",f1_score(y_test,y_predicted))
+print("Confusion Matrix:",confusion_matrix(y_test,y_predicted))
+
+
+print("\n ____________________________Training Scratch Logistic Regression ___________________________")
+scratch_model = Scratch_Logistic_Regression(learning_rate=0.1, iterations=1000)
+
+scratch_model.fit(X_train_scaled, y_train.to_numpy()) 
+scratch_preds = scratch_model.predict(X_test_scaled)
+print("Predictions:",scratch_preds[:10])
+print("--- Scratch Model Performance ---")
+print(f"Accuracy: {accuracy_score(y_test, scratch_preds):.4f}")
+print(f"F1 Score: {f1_score(y_test, scratch_preds):.4f}")
+print("Confusion Matrix:\n", confusion_matrix(y_test, scratch_preds))
